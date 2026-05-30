dbd5edc14d2ce971933ce0a5f0121cbd0b33e5cd
Joel Lovely <Joellovely0717@gmail.com>
2026-05-29 19:29:49 -0500

    update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from wtforms import DecimalField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 # Constants
-THE_MOVIES_DATABASE_API_KEY = "175dfd7672f3e6dee79b18aafc91b39f"
+THE_MOVIES_DATABASE_API_KEY = os.environ.get("TOP_10_MOVIES_API_KEY")
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 THE_MOVIES_DATABASE_URL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
