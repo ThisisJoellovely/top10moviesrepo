4e2e149ef03db3e088bf9f738a3963e4a0c3babb
Joel Lovely <Joellovely0717@gmail.com>
2026-05-29 19:00:16 -0500

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from wtforms import DecimalField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
-load_dotenv("/.env")
+load_dotenv(".env")
 
 # Constants
 THE_MOVIES_DATABASE_API_KEY = os.getenv("TOP_10_MOVIES_API_KEY")
